31c2328fff8ccb0f9164221537575ce1a53f89ed
OMAR404 <omar97.hassan97@gmail.com>
2022-05-04 16:16:25 +0200

    show alert message when user join another room

--- screens/JoinRoom.js
+++ screens/JoinRoom.js
@@ -1,9 +1,9 @@
 import React, { useContext } from "react";
-import { FlatList, Image, StyleSheet, View } from "react-native";
+import { Alert, FlatList, Image, StyleSheet, View } from "react-native";
 import CreatedRoom from "../components/CreatedRoom";
 import { RoomsContext } from "../context/RoomsContext";
 import { TeamsContext } from "../context/TeamsContext";
-import { roomRef, userRef } from "../service/firebase";
+import firebaseService, { roomRef, userRef } from "../service/firebase";
 import { update } from "firebase/database";
 import * as _ from "lodash";
 import { useSession } from "../context/SessionContext";
@@ -15,29 +15,46 @@ const JoinRoom = ({ navigation }) => {
   const user = useSession()
   const userTeam = teams?.[user?.team] || {}
   const { id: userTeamId, divisionRecords } = userTeam
-  const { DIV: teamDivision } = divisionRecords || ""
-  const onJoinRoomPress = (room) => {
-    if (room.status == 'full' && (room.homeTeam != user.team && room.awayTeam != user.team))
-      return
-    update(userRef(user.id), { joinedRoomId: room.id })
-    if (room.homeTeam == user.team) {
-      update(roomRef(`${teamDivision}/${room.id}`), {
-        homeTeam: userTeamId,
-        status: "full"
-      });
-    } else {
-      update(roomRef(`${teamDivision}/${room.id}`), {
-        awayTeam: userTeamId,
-        status: "full"
-      });
+  const { DIV: teamDivision } = divisionRecords || {}
+
+  const handleJoinRoom = (room) => {
+    if (room.homeTeam != user.team) {
+      firebaseService.joinRoom(teamDivision, room.id)
     }
     navigation.push('Room', { roomId: room.id, user, teamDivision })
+  }
+  const onJoinRoomPress = (room) => {
+    // user cannot join because  room is full and room teams aren't user team
+    if (room.status == 'full' && (room.homeTeam != user.team && room.awayTeam != user.team))
+      return
+    // user can join
+    // -> but already joined room
+    if (user?.joinedRoomId && user?.joinedRoomId != room?.id) {
+      Alert.alert('you already in a room do you want to leave it and join this room ?', '', [
+        {
+          text: 'no'
+        },
+        {
+          text: 'yes',
+          onPress: () => {
+            firebaseService.leaveRoom(teamDivision, user?.joinedRoomId)
+            handleJoinRoom(room)
+          }
+        }
+      ])
+    } else {
+      handleJoinRoom(room)
+    }
+
   };
   navigation.setOptions({
     headerRight: () => (
       <View style={{ flexDirection: 'row', alignItems: 'center' }}>
         <Image style={{ width: 30, height: 30 }} source={{ uri: userTeam.logoUrl }} />
-        <Text style={{ marginHorizontal: 12 }}>{userTeam.name}</Text>
+        <View>
+          <Text style={{ marginHorizontal: 12 }}>{userTeam.name}</Text>
+          <Text style={{ marginHorizontal: 12 }}>DIV: {userTeam.divisionRecords.DIV}</Text>
+        </View>
       </View>
     )
 
--- service/firebase.js
+++ service/firebase.js
@@ -1,7 +1,38 @@
-import { getDatabase, ref, onValue, set } from "firebase/database";
+import { getDatabase, ref, remove, update, set } from "firebase/database";
 const db = getDatabase();
 
+// db refs
 export const roomsRef = ref(db, "Rooms");
 export const roomRef = path => ref(db, `Rooms/${path}`);
 export const teamsRef = ref(db, "Teams");
 export const usersRef = ref(db, "Users")
+export const userRef = id => ref(db, `Users/${id}`)
+
+
+export default (function firebaseService() {
+  let user;
+  return {
+    setUser: _user => user = _user,
+    leaveRoom: (teamDivision, roomId) => {
+      console.log(teamDivision)
+      console.log(roomId)
+      console.log(user.id)
+      remove(roomRef(`${teamDivision}/${roomId}/awayTeam`)).then(res => {
+        console.log('remove away team resolved')
+      })
+      remove(userRef(`${user.id}/joinedRoomId`)).then(res => {
+        console.log('remove joinedRoomId resolved')
+      })
+      update(roomRef(`${teamDivision}/${roomId}`), { status: 'empty', awayTeamReady: false }).then(res => {
+        console.log('update status resolved')
+      })
+    },
+    joinRoom: (teamDivision, roomId) => {
+      update(userRef(user.id), { joinedRoomId: roomId })
+      update(roomRef(`${teamDivision}/${roomId}`), {
+        awayTeam: user.team,
+        status: "full"
+      });
+    }
+  }
+})()
